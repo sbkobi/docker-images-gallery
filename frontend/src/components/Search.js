4605ecb03cf1092e554b26a392c04c2be992692b
sbkobi <ybesig@gmail.com>
2023-09-18 13:04:13 +0300

    added mongo and mongo-express to docker-compse

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -18,7 +18,7 @@ const Search = ({ word, setWord, handleSubmit }) => {
               </Col>
               <Col>
                 <Button variant="primary" type="submit">
-                  Search
+                  Search jack
                 </Button>
               </Col>
             </Form.Row>
